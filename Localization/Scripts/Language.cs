7fd93ac5b16ec6c057df1f18e81b82aea036d0b8
LobovichArtem <83509416+LobovichArtem@users.noreply.github.com>
2024-09-19 22:30:53 +0300

    Update Language.cs

--- Localization/Scripts/Language.cs
+++ Localization/Scripts/Language.cs
@@ -4,22 +4,12 @@ using UnityEngine.UI;
 
 public class Language : MonoBehaviour
 {
+    [SerializeField] private LanguageCode _language;
     [SerializeField] private Text _currentLanguageCode; 
-    
+
     private void Awake()
     {
         SetLanguage(LoadLanguageCode);
-    }
-
-    public void SetLanguage(int code)
-    {
-        if (IsValidLanguage(code) == false)
-            throw new Exception("Invalid language code value.The value is greater than the enum languageCode");
-
-        Localization.SetLanguage((LanguageCode)code);
-        SaveLanguageCode(code);
-        
-        DisplayCurrentLanguage();    
     }
 
     public void SwitchLanguage()
@@ -31,16 +21,21 @@ public class Language : MonoBehaviour
         SetLanguage(code);
     }
 
+    public void SetLanguage(int code)
+    {
+        if (IsValidLanguage(code) == false)
+            throw new Exception("Invalid language code value.The value is greater than the enum languageCode");
+
+        Localization.SetLanguage((LanguageCode)code);
+        _language = (LanguageCode)code;
+        SaveLanguageCode(code);
+        _currentLanguageCode.text = Localization.Language.ToString();
+    }
+
     [ContextMenu("UpdateFiles")]
     public void UpdateFiles()
     {
         Localization.UpdateResources();
-    }
-
-    public void DisplayCurrentLanguage()
-    {
-        if(_currentLanguageCode != null) 
-            _currentLanguageCode.text = Localization.Language.ToString();    
     }
 
     [ContextMenu("SaveCurrentLanguage")]
@@ -51,4 +46,5 @@ public class Language : MonoBehaviour
     private void SaveLanguageCode(int language) => PlayerPrefs.SetInt("languagecode", language);
 
     private bool IsValidLanguage(int code) => code < Enum.GetValues(typeof(LanguageCode)).Length;
+
 }
